25c9fd83eb641c9d9dd559c6ad4943d9b5df3a74
boven333 <112691124+boven333@users.noreply.github.com>
2023-10-18 00:07:26 +0700

    added addEmployees functionality

--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -89,6 +89,7 @@ const Dashboard = ({ setIsAuthenticated }) => {
           employees={employees}
           setEmployees={setEmployees}
           setIsAdding={setIsAdding}
+          getEmployees={getEmployees}
         />
       )}
       {isEditing && (
